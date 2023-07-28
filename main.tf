96c86ae931ec448f1352b3eb980deb90deb68529
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2023-07-28 11:28:17 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+variable "broika" {
+default = {"edno","dve","tri"}
+
+resource "null_resource" "ivan" {
+for_each = var.broika
+provisioner "local-exec" {
+    command = <<EOT
+    echo 'Hello ivan'
+    EOT
+  }
+}
+
+
